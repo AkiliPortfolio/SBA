f003ad1631b9976bec6222c85d6a987c175962a8
AkiliPortfolio <145647661+AkiliPortfolio@users.noreply.github.com>
2023-10-12 15:27:56 +0800

    12 oct 2023 SBA

---
+++ script.js
@@ -0,0 +1,19 @@
+const thumbnails = document.querySelectorAll('.video-thumbnails img');
+const video = document.querySelector('.video-youtube iframe');
+const videoInfo = document.querySelector('.video-info');
+
+thumbnails.forEach(function(thumbnail) {
+ thumbnail.addEventListener('click', function() {
+   const videoId = this.getAttribute('data-video-id');
+   const thumbnailSrc = this.getAttribute('src');
+   const videoTitle = this.getAttribute('alt');
+   const videoDescription = this.getAttribute('data-description');
+   video.setAttribute<iframe width="560" height="315" src="https://www.youtube.com/embed/zaNh3dClwxI?si=D5l1KqzmmgkFxdb0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   videoInfo.querySelector('h2').textContent = videoTitle;
+   videoInfo.querySelector('p').textContent = videoDescription;
+   thumbnails.forEach(function(thumbnail) {
+     thumbnail.classList.remove('active');
+   });
+   this.classList.add('active');
+ });
+});
